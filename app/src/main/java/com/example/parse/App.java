5563e556ff7b5171414daa44a06c70762adaa87b
savanakbari <savan.akbari@gmail.com>
2016-01-09 13:42:21 -0600

    Final Commit

--- app/src/main/java/com/example/parse/App.java
+++ app/src/main/java/com/example/parse/App.java
@@ -1,8 +1,10 @@
 package com.example.parse;
 
 import android.app.Application;
+import android.content.Intent;
 
 import com.parse.Parse;
+import com.parse.ParseUser;
 
 
 public class App extends Application {
@@ -14,5 +16,13 @@ public class App extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "LeAZUl1WfJWgfWkaVCv79LqSyEBeXUaFsKi5iQQU", "sr9ReTPzHbi4SVExat2ULEfFhCz02sJxMVB4byMd");
+
+        if(ParseUser.getCurrentUser()!= null){
+            Intent intent= new Intent(this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+
+        }
     }
 }
